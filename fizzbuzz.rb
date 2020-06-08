a2b9d5bf76c06e7b96a690a3e764628ef5cdc146
Michael Waide <mvwaide@gmail.com>
2020-06-08 16:29:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
-  if i % 3 == 0 && i % 5 == 0
+  if int % 3 == 0 && i % 5 == 0
      puts "FizzBuzz"
-   elsif i % 3 == 0
+   elsif int % 3 == 0
      puts "Fizz"
-   elsif i %  5 == 0
+   elsif int %  5 == 0
      puts "Buzz"
    else
-      puts i
+      puts int
   end
 end
