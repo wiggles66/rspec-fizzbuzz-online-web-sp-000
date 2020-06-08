3d6b8c04c7ff82c308026c891ea23cffb48fd22d
Michael Waide <mvwaide@gmail.com>
2020-06-08 16:39:52 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -18,7 +18,7 @@ describe "fizzbuzz" do
     expect(fizz_15).to eq("FizzBuzz")
   end
   it 'returns nil when the number is not divisible by 3 or 5' do
-    fizz_4 = fizzbuzz(4)
+    fizz_4 = fizzbuzz(nil)
 
     expect(fizz_4).to eq(nil)
   end
