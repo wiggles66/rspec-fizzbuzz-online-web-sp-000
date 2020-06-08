06a727ee4f33122268f7170cdcad3a45ac9f0daa
Michael Waide <mvwaide@gmail.com>
2020-06-08 16:16:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,9 @@ def fizzbuzz
 1.up(100) do |i|
   if i % 3 = 0 && i % 5 = 0
      puts "FizzBuzz"
-   elsif i % 3 == 0
+   elsif i % 3 = 0
      puts "Fizz"
-   elsif i %  5 == 0
+   elsif i %  5 = 0
      puts "Buzz"
    else
       puts i
